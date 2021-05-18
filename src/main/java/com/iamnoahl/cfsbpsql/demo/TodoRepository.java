d0e1e726706c00043ef6ac4034f11115dadb6d29
Noah Ispas <noi@dataone.de>
2021-05-18 10:42:27 +0200

    remove step 5 solution

--- src/main/java/com/iamnoahl/cfsbpsql/demo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iamnoahl.cfsbpsql.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TodoRepository extends JpaRepository<TodoModel, UUID> {
-}
